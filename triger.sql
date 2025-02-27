ed5bb3970ba1bdff2a514bacc594c6c555cec879
jul213 <158296107+jul213@users.noreply.github.com>
2025-02-27 20:27:52 +0000

    mejor usamos bit en vez de boolean

--- triger.sql
+++ triger.sql
@@ -3,7 +3,7 @@ on      empleado
 for insert  
 as
 begin
- declare @edad int, @alta boolean, @lider boolean
+ declare @edad int, @alta boolean, @lider bit 
  select @edad= edad, @alta = fecha_alta, @lider = lider from inserted 
  update empleado set lider = !@lider where edad = @edad
 
