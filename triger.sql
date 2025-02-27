70cb2ec389f53a58f477f3ecb7b98463341609c1
jul213 <158296107+jul213@users.noreply.github.com>
2025-02-27 20:30:56 +0000

    condicional if

--- triger.sql
+++ triger.sql
@@ -5,7 +5,10 @@ as
 begin
  declare @edad int, @lider bit 
  select @edad= edad, @lider = lider from inserted 
- update empleado set lider = 0 
- where edad = @edad and id_empleado in (select id_empleado from inserted);
-
+ if @edad < 25
+ begin 
+    update empleado 
+    set lider = 0 
+    where edad = @edad and id_empleado in (select id_empleado from inserted);
+ end
 end
