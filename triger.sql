69e3d59a1f17b181bbc261c68eb9289e94ec4576
jul213 <158296107+jul213@users.noreply.github.com>
2025-02-27 20:27:31 +0000

    correcion de error de insnerted

--- triger.sql
+++ triger.sql
@@ -4,7 +4,7 @@ for insert
 as
 begin
  declare @edad int, @alta boolean, @lider boolean
- select @edad= edad, @alta = fecha_alta, @lider = lider from empleado 
+ select @edad= edad, @alta = fecha_alta, @lider = lider from inserted 
  update empleado set lider = !@lider where edad = @edad
 
 end
