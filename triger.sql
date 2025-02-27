10c273bf99b9ee22df6b15533b39797082620453
jul213 <158296107+jul213@users.noreply.github.com>
2025-02-27 20:29:43 +0000

    mejoramos el trigger

--- triger.sql
+++ triger.sql
@@ -3,8 +3,9 @@ on      empleado
 for insert  
 as
 begin
- declare @edad int, @alta boolean, @lider bit 
- select @edad= edad, @alta = fecha_alta, @lider = lider from inserted 
- update empleado set lider = !@lider where edad = @edad
+ declare @edad int, @lider bit 
+ select @edad= edad, @lider = lider from inserted 
+ update empleado set lider = 0 
+ where edad = @edad and id_empleado in (select id_empleado from inserted);
 
 end
