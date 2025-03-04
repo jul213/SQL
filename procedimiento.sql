cd878d843b4c401c1fbb8e82c9388f22f747c54e
jul213 <158296107+jul213@users.noreply.github.com>
2025-03-04 21:05:48 +0000

    insert

--- procedimiento.sql
+++ procedimiento.sql
@@ -17,3 +17,7 @@ end
 create procedure insert_u @CustomerID char(5),
                           @CompanyName varchar(40),
                           @returnID char(5) OUTPUT
+as
+begin 
+insert into Customers(CustomerID, CompanyName) VALUES (CustomerID,  @ CompanyName)
+if ()
