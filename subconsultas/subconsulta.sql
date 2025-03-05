8ca22296954e7ea67cb72acaa266e28f1b019039
jul213 <158296107+jul213@users.noreply.github.com>
2025-03-05 21:00:28 +0000

    terminamos subconsulta

--- subconsultas/subconsulta.sql
+++ subconsultas/subconsulta.sql
@@ -2,4 +2,4 @@ select  od.date, p.name
 from orderdetails od
 inner join products p on od.productID = p.productID
 where "usa" in (select country from suppliers
-                where products.supplierID = supplier.ID )
+                where products.supplierID = supplier.ID from suppliers)
