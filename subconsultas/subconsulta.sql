d4e9b97059e77066dff4aa348eb331170e0e1b76
jul213 <158296107+jul213@users.noreply.github.com>
2025-03-05 21:07:03 +0000

    correcion de comillas dobles a simmples

--- subconsultas/subconsulta.sql
+++ subconsultas/subconsulta.sql
@@ -1,6 +1,6 @@
 select  od.date, p.name 
 from orderdetails od
 inner join products p on od.productID = p.productID
-where "usa" in (select s.country from suppliers s
+where 'usa' in (select s.country from suppliers s
                 where p.supplierID = s.supplierID)
 and p.productName like 'L%'
