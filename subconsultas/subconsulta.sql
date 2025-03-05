ec7f30f37edeed584fc91b42a4b86b1ef28cbb84
jul213 <158296107+jul213@users.noreply.github.com>
2025-03-05 21:05:41 +0000

    correcion de la subconsulta

--- subconsultas/subconsulta.sql
+++ subconsultas/subconsulta.sql
@@ -1,6 +1,6 @@
 select  od.date, p.name 
 from orderdetails od
 inner join products p on od.productID = p.productID
-where "usa" in (select country from suppliers
-                where products.supplierID = supplier.ID from suppliers)
-and productName like 'L%'
+where "usa" in (select s.country from suppliers s
+                where p.supplierID = s.supplierID)
+and p.productName like 'L%'
